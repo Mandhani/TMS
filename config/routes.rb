179ef5d562b5408715b227cad6c4f4cac1c3a527
Mandhani Kushal <mkushal@ncsu.edu>
2019-02-18 16:06:54 -0500

    Fixing-index-page-authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/edit'
+  get 'users/new'
+  get 'users/show'
+  get 'users/create'
+  get 'users/update'
+  get 'users/destroy'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => 'devise/sessions#new'
+  root to: redirect('users/index')
 end
