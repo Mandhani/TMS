6e1888ac6c27d6289ba84cdcaad04033fc5d3555
Mandhani Kushal <mkushal@ncsu.edu>
2019-02-26 20:32:43 -0500

    user-model-implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   # attr_accessible :user_type
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  # Reference: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, on: :create}
 end
 
